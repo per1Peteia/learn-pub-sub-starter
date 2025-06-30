f43e15cce18dc5ec1a56e986bcfee54d9f93289a
per1Peteia <justusmanthey@googlemail.com>
2025-06-30 16:34:16 +0200

    feat: client creates and deletes transient queue automatically

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/per1Peteia/learn-pub-sub-starter/internal/gamelogic"
+
+	"github.com/per1Peteia/learn-pub-sub-starter/internal/pubsub"
+	"github.com/per1Peteia/learn-pub-sub-starter/internal/routing"
+
+	"github.com/rabbitmq/amqp091-go"
+)
+
+const CONN_URL = "amqp://guest:guest@localhost:5672/"
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	conn, err := amqp091.Dial(CONN_URL)
+	if err != nil {
+		log.Fatalf("Error dialing: %v", err)
+	}
+
+	defer conn.Close()
+	fmt.Println("Connection successful")
+
+	userName, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("Error getting username: %v", err)
+	}
+
+	_, _, err = pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, fmt.Sprintf("%s.%s", routing.PauseKey, userName), routing.PauseKey, 1)
+	if err != nil {
+		log.Fatalf("Error declaring and binding queue to channel: %v", err)
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("RabbitMQ connection closed.")
 }
